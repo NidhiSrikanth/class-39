23adc7509bdbdc9671536b34f1cb3dadb80b0b0f
NidhiSrikanth <65636801+NidhiSrikanth@users.noreply.github.com>
2020-07-13 13:47:36 -0400

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,55 @@
+class Form {
+    constructor(){
+        
+        this.input= createInput("name");
+        this.button= createButton("play");
+        this. greeting= createElement("h3");
+        this.reset= createButton("reset");
+
+    }
+
+    display(){
+        var title= createElement("h2");
+        title.html("Car Racing Game");
+        title.position(displayWidth/2 -50,0);
+
+        
+        this.input.position(displayWidth/2 -40,displayHeight/2 -80);
+
+        
+        this.button.position(displayWidth/2 +40,displayHeight/2);
+
+        this.reset.position(displayWidth-100,20);
+
+       
+
+        this.button.mousePressed( ()=>{
+            this.input.hide();
+            this.button.hide();
+            player.name= this.input.value();
+
+            playerCount= playerCount+1;
+            player.index= playerCount;
+            player.updateName ();
+            player.updateCount(playerCount);
+
+            this.greeting.html("Welcome"+ player.name);
+            this.greeting.position(displayWidth/2 -80,displayHeight/4 );
+        })
+
+
+        this.reset.mousePressed(()=>{
+        player.updateCount(0);
+        game.update(0);
+        })
+    }
+
+    
+
+    hide(){
+        this.greeting.hide();
+        this.button.hide();
+        this.input.hide();
+        
+    }
+}
